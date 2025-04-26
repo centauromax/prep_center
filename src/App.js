51c8aea0fc97edc22e246478978216ae3ef7d5f6
Adriano Zancan <adrzan.mbox@yahoo.it>
2025-04-26 07:27:46 +0200

    Ritorno alla struttura Django standard

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Picture Check App</h1>
+        <p>App per la verifica delle foto dei prodotti</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
