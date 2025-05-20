6940428761e0b8b3d72f30b90b3a04893f63c952
Adriano Zancan <adrzan.mbox@yahoo.it>
2025-05-20 10:21:59 +0200

    Ricerca spedizioni

--- backend/prep_management/migrations/0009_remove_shipment_id_api.py
+++ backend/prep_management/migrations/0009_remove_shipment_id_api.py
@@ -3,7 +3,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('prep_management', '0008_fix_searchresultitem_model'),
+        ('prep_management', '0007_searchresultitem_processing_status_and_more'),
     ]
 
     operations = [
